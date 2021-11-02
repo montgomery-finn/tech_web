c2c8fa7f5d603618640a017bcc556ac60e94207f
Sandro Junior <sjr1999@protonmail.com>
2021-11-02 11:47:28 -0300

    Add previous code

---
+++ src/pages/signup/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Card, Button, Form} from 'react-bootstrap';
+import {Container} from './styles';
+
+const Signup: React.FC = () => (
+  <Container>
+      <Card>
+        <Card.Body className="d-flex justify-content-center">
+          <Button >
+            asdasdasd
+          </Button>
+        </Card.Body>
+      </Card>
+  </Container>)
+
+export default Signup;
